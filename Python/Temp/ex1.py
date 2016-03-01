3215d2800776b0da347d49eeba86990a55c4d779
bipinbachhao <bipinbachhao@gmail.com>
2016-02-29 20:53:25 -0500

    Added comments

--- Python/Temp/ex1.py
+++ Python/Temp/ex1.py
@@ -1,7 +1,9 @@
-print "Hello World This is first python code from windows."
+# this is comment is python
+# anything typed after # will be iqnored 
+print "Hello World This is first python code from windows.\n"
 print "Hello again."
 print "I like to type this."
 print "this is fun."
-print 'Yay! Printing.'
+print 'Yay! \tPrinting.'
 print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
