e63a940c73a2f93a49c49a0ca601c9f55980446b
bipinbachhao <bipinbachhao@gmail.com>
2016-03-08 22:10:39 -0500

    Exercises

---
+++ Python/Temp/ex3.py
@@ -0,0 +1,18 @@
+print "I will count my Chickens:"
+print "Hens", 25 + 30 /6
+print "Roosters", 100 - 25 * 3 % 4
+print "Now i will count eggs:"
+print 3 + 2 + 1 - 5  + 4 % 2 - 1 / 4 + 6
+print "Is it true that  3 + 2 < 5 - 7?"
+print 3 + 2 < 5 - 7
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7 
+print "Oh, that's why it's False."
+print "Is it greater ?", 5 > -2
+print "Is it greater than or equal?", 5 >= -2
+print "Is it less or equal", 5 <= -2
+print """ Bipin bachhao
+bipin bachhai
+bipin
+djfklsjfds
+"""
